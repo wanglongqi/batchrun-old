8d2c756d7234a48e0f83eadc7e56071295ea735f
wlongqi@gmail.com <wlongqi@gmail.com@228784da-5693-6413-db0d-2856989a1015>
2012-04-11 08:09:49 +0000

    提交错代码了

--- trunk/batchrun.py
+++ trunk/batchrun.py
@@ -6,6 +6,7 @@ Created on 2011-4-11
 '''
 import sys
 import optparse
+import time
 
 def dofile(x):
     import os
@@ -29,7 +30,7 @@ options, others = parser.parse_args()
 if others==[]:    
     others=[None]    
 import subprocess
-if options.ncpu !=None:
+if options.ncpu ==None:
     import multiprocessing
     options.ncpu=multiprocessing.cpu_count()
 
@@ -41,16 +42,12 @@ for file in others:
     jobs=[]
     jcount=0
     for line in fid:
-        if options.verbosity>1:
+        if options.verbosity>0:
             jcount+=1
             print 'Jobs',jcount,':',line,
         jobs.append(line)
 
-if options.verbosity>1 and jcount>0:
-    count=0
-    for job in jobs:
-        print 'Job ',count,' :',job
-        count+=1
+if options.verbosity>2 and jcount>0:
     print '\nDo you want to execute the jobs?(y/N)'
     if raw_input()=='N':
         sys.exit()
@@ -62,26 +59,29 @@ while True:
     try:
         # Add jobs
         while len(rjob)<options.ncpu:
-            rjob.append(subprocess.Popen(jobs[jcount]),shell=True)
-            rinfo.append((jobs[count],time.clock(),rjob[-1].pid,jcount))
-            if options.verbosity>2:
+            if jcount>=len(jobs):
+                break
+            rjob.append(subprocess.Popen(jobs[jcount],shell=True))
+            rinfo.append((jobs[jcount],time.clock(),rjob[-1].pid,jcount))
+            if options.verbosity>1:
                 print 'Executing job',jcount,', Pid is',rjob[-1].pid
             if options.verbosity>3:
-                print ':: New Job info :: \n Command: %s \t %Start time: %g\n Job pid: %d\tJob count: %d'%(rinfo[-1][0],rinfo[-1][1],rinfo[-1][2],rinfo[-1][3])
+                print ':: New Job info :: \n Command: %s \t Start time: %g\n Job pid: %d\tJob count: %d'%(rinfo[-1][0],rinfo[-1][1],rinfo[-1][2],rinfo[-1][3])
             jcount+=1
-            if jcount>len(jobs):
-                break
+        
+        if len(rjob)==0:
+            break
         
         # Remove finished jobs
-        for i in range(len(rjob)):
+        for i in range(len(rjob)-1,-1,-1):
             p=rjob[i].poll()
             if p!=None:
                 if p!=0:
                     print rinfo[i][1],'exit abnormal. Exit code',p
-                if options.verbosity>2:
-                    print 'Job %d Finished!'%rinfo[i][3]
+                if options.verbosity>1:
+                    print 'Job %d Finished! Total time %g Sec.'%(rinfo[i][3],time.clock()-rinfo[-1][1])
                 if options.verbosity>3:
-                    print ':: Finished Job info :: \n Command: %s \t %Total time: %g\n Job pid: %d\tJob count: %d'%(rinfo[-1][0],time.clock()-rinfo[-1][1],rinfo[-1][2],rinfo[-1][3])
+                    print ':: Finished Job info :: \n Command: %s \t Total time: %g\n Job pid: %d\tJob count: %d'%(rinfo[-1][0],time.clock()-rinfo[-1][1],rinfo[-1][2],rinfo[-1][3])
                 rjob.pop(i)
                 rinfo.pop(i)
                 
@@ -96,3 +96,6 @@ while True:
         print jcount,'jobs has been submitted. Rest jobs will be abandoned.'
         print 'Submited job will not be affected. Kill the process if you will.'
         pass
+    except Exception,msg:
+        print msg
+        raise
